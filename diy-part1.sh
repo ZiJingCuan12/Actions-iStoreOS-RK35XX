8f3fdf31a90b5a30ff55efa06d01077c38b79f1b
ZiJingCuan12 <cpy13951041292020@gmail.com>
2025-12-15 23:57:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,23 @@
 date_version=$(date +"%Y%m%d%H")
 echo $date_version > version
 
-# 为iStoreOS固件版本加上编译作者
-author="xiaomeng9597"
+# ---------------------------------------------------------
+# [修改点 1] 设置你的品牌名称
+# ---------------------------------------------------------
+# 将作者名改为 MuFVps，这样版本描述中会显示 "by MuFVps"
+author="MuFVps"
+
+# ---------------------------------------------------------
+# [修改点 2] 修改系统底层标识 ID
+# ---------------------------------------------------------
+# 这行命令非常重要，它配合 diy-part2 的修改，确保系统认为自己是 MuFVps
+# 注意：这行必须放在 package/base-files 相关操作之前
+sed -i "s/DISTRIB_ID=.*/DISTRIB_ID='MuFVps'/g" package/base-files/files/etc/openwrt_release
+
+# 修改系统描述和发布信息
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version} by ${author}'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}\"/g" package/base-files/files/usr/lib/os-release
 
 # 拉取我的软件包仓库
+# (保留原逻辑，如果你有自己的仓库也可以替换这里)
 echo 'src-git xmpackages https://github.com/xiaomeng9597/openwrt-packages2.git;main' >> feeds.conf.default
